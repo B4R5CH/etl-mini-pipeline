7a082956a7c582b7ca141d9dd38f65040b771ad9
realFvalue <tommy.cannon2@icloud.com>
2026-06-11 21:57:32 +0100

    added additional queries to the pack

--- analytics_queries.sql
+++ analytics_queries.sql
@@ -24,3 +24,32 @@ FROM fact_transactions
 JOIN dim_run
     ON fact_transactions.run_key = dim_run.run_key
 GROUP BY dim_run.run_id;
+
+-- average transaction by currency
+
+SELECT
+    dim_currency.currency_code,
+    AVG(fact_transactions.amount) AS average_amount
+FROM fact_transactions
+JOIN dim_currency
+    ON fact_transactions.currency_key = dim_currency.currency_key
+GROUP BY dim_currency.currency_code;
+
+-- transaction count by run
+
+SELECT
+    dim_run.run_id,
+    COUNT(*) AS run_count
+FROM fact_transactions
+JOIN dim_run
+    ON fact_transactions.run_key = dim_run.run_key
+GROUP BY dim_run.run_id;
+
+
+
+-- fact table verification query
+SELECT COUNT(*) AS fact_rows
+FROM fact_transactions;
+
+SELECT COUNT(*) AS source_rows
+FROM clean_transactions;
